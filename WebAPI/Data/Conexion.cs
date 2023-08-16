e462efc738272d01b1fcd6952c6d1fdf082d51f6
usuario <usuario@DESKTOP-KKCDNM3>
2023-08-16 18:11:25 -0500

    Agregar archivos de proyecto.

---
+++ WebAPI/Data/Conexion.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebAPI.Data
+{
+    public class Conexion
+    {
+
+        public static string rutaConexion = "Data Source=DESKTOP-KKCDNM3\\SQLEXPRESS;Initial Catalog=DBPRUEBAS;Integrated Security=True";
+
+    }
+}
